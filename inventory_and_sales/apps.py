e7a8896d29ff16ffd8886c037988a3b886e410e6
Raghav <raghav.chopra@zenatix.com>
2020-05-30 09:31:38 +0530

    basic setup

---
+++ inventory_and_sales/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class InventoryAndSalesConfig(AppConfig):
+    name = 'inventory_and_sales'
