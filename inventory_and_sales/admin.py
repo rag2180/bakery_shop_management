2a74ee52d9d4c6615c75a5087f60145ab5f96bcc
Raghav <raghav.chopra@zenatix.com>
2020-05-30 22:18:41 +0530

    models modified

--- inventory_and_sales/admin.py
+++ inventory_and_sales/admin.py
@@ -4,10 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer, Order, Product, ProductOrder, Category
+from .models import Customer, Order, Product, Category, OrderItems
 
 admin.site.register(Customer)
 admin.site.register(Order)
 admin.site.register(Product)
-admin.site.register(ProductOrder)
+admin.site.register(OrderItems)
 admin.site.register(Category)
--- inventory_and_sales/models.py
+++ inventory_and_sales/models.py
@@ -3,43 +3,60 @@ from __future__ import unicode_literals
 
 from django.core.validators import RegexValidator
 from django.db import models
+from datetime import datetime
 
 
 class Customer(models.Model):
     name = models.CharField(verbose_name="Name", help_text="Name of the customer", max_length=255, unique=False)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)  # validators should be a list
-    address = models.CharField(verbose_name="Address", help_text="Address of Customer", default=None, null=True, max_length=255)
-    city = models.TextField(help_text="Name of City to which customer belongs", max_length=255)
-    email_id = models.EmailField(null=True)
+    address = models.CharField(verbose_name="Address", help_text="Address of Customer", default=None, null=True, max_length=255, blank=True)
+    city = models.CharField(help_text="Name of City to which customer belongs", max_length=255, blank=True)
+    email_id = models.EmailField(null=True, blank=True)
     date = models.DateField(help_text="Date when customer was acquired", null=True, auto_now_add=True)
+
+    def __str__(self):
+        return "{}|{}".format(self.name, self.phone_number)
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, help_text="Category of a product. Eg: Raw/Final")
+    name = models.CharField(max_length=255, help_text="Category of a product. Eg: Raw/Final", unique=True)
     description = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
     name = models.CharField(help_text="Name of Item", max_length=255, unique=True)
+    # image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)  # blank means its not required in django admin
     category = models.ForeignKey(Category, help_text="Category of this product item")
     quantity = models.IntegerField(help_text="Quantity of item stored")
-    unit = models.CharField(help_text="units of quantity", max_length=255)
-    cost_price = models.IntegerField(name="Cost Price", help_text="Cost of your own purchase or cost of production of this product", null=True)
-    selling_price = models.IntegerField(name="Selling Price", help_text="Selling price of product", null=True)
+    unit = models.CharField(help_text="units of quantity", max_length=255, null=True, blank=True)
+    cost_price = models.IntegerField(name="Cost Price", help_text="Cost of your own purchase/cost of production of this product", null=True, blank=True)
+    selling_price = models.IntegerField(name="Selling Price", help_text="Selling price of product", null=True, blank=True)
+
+    def __str__(self):
+        # return self.name
+        return "{}|{}".format(self.name, self.category)
 
 
 class Order(models.Model):
-    customer_id = models.ForeignKey(Customer)
+    customer = models.ForeignKey(Customer)
     delivery_status = models.BooleanField(help_text="Is the order delivered?")
     payment_status = models.BooleanField(help_text="Is the payment received from customer?")
-    total_amount = models.IntegerField(name="Order Value")
+    order_datetime = models.DateTimeField(name="Datetime", help_text="Date and time when order was placed", null=True, default=datetime.now())
+    total_price = models.FloatField(verbose_name="Total Amount", default=0.0, null=True)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} - {}".format(self.id, self.customer.name)
 
 
-class ProductOrder(models.Model):
+class OrderItems(models.Model):
     order_id = models.ForeignKey(Order)
     product_id = models.ForeignKey(Product)
     quantity = models.IntegerField(name="Quantity", help_text="units of product sold to customer", null=True)
-    price = models.IntegerField(name="Price", help_text="This is product of product price and quantity", null=True)
 
-
+    def __str__(self):
+        return "{} | {}".format(self.id, self.product_id)
