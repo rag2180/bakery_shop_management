0561cb5dc6811c05800c16b5c170fbcb70a046fe
Raghav <raghav.chopra@zenatix.com>
2020-05-31 20:10:36 +0530

    set secret key to heroku environment variable and using that in settings

--- bakery_shop/settings.py
+++ bakery_shop/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'higrko6h1r+6!#^s^_r*o#*zy^wyd0o(uw-$%+rt++=pt+xck7'
+# SECRET_KEY = 'higrko6h1r+6!#^s^_r*o#*zy^wyd0o(uw-$%+rt++=pt+xck7'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
